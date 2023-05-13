78d4fbbad73dcfce62c7d67ebc06d5efe7fbd20f
sec-study-dev <feng.luo.lf@gmail.com>
2023-05-13 14:36:05 +0800

    update

--- attackDetector/detecor.py
+++ attackDetector/detecor.py
@@ -6,28 +6,145 @@ def build_chain(blockchain):
 		cwd = os.getcwd()
 		install_path = os.path.join(cwd, "attackDetector", "attack_configuration", "eos_script", "install_eosio.sh")
 		os.chdir(os.path.dirname(install_path))
-		os.system("./install_eosio.sh")
+
+		try:
+			os.system("./install_eosio.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./import_wallet.sh")
+
+		try:
+			os.system("./import_wallet.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./genesis_start.sh")
+
+		try:
+			os.system("./genesis_start.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./deploy_contract.sh")
+
+		try:
+			os.system("./deploy_contract.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./after_deploy.sh")
+
+		try:
+			os.system("./after_deploy.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./allocate_bp.sh")
+
+		try:
+			os.system("./allocate_bp.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./transfer_bp.sh")
+
+		try:
+			os.system("./transfer_bp.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 		time.sleep(1)
-		os.system("./return_pri.sh")
+
+		try:
+			os.system("./return_pri.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+def attack(blockchain):
+	if blockchain == "eos":
+		# 1. SCP resource drain
+		# cpu drain attack
+		cwd = os.getcwd()
+		cpu_path = os.path.join(cwd, "attackDetector", "attack_configuration", "eos_dos_contract", "cpu_drain_attack", "cpu_victim")
+		os.chdir(os.path.dirname(cpu_path))
+
+		try:
+			os.system("./cpu_victim/bianyi.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./create_account.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./deploy_victim.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./attack_all.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		# ram drain attack
+		cwd = os.getcwd()
+		cpu_path = os.path.join(cwd, "attackDetector", "attack_configuration", "eos_dos_contract", "ram_drain_attack", "ram_victim")
+		os.chdir(os.path.dirname(cpu_path))
+
+		try:
+			os.system("./ram_victim/bianyi.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./create_account.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./deploy_victim.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./attack_all.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+def end(blockchain):
+	if blockchain == "eos":
+		cwd = os.getcwd()
+		stop_path = os.path.join(cwd, "attackDetector", "attack_configuration", "eos_script", "stop_all.sh")
+		os.chdir(os.path.dirname(stop_path))
+
+		try:
+			os.system("./stop_all.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
+
+		time.sleep(1)
+
+		try:
+			os.system("./uninstall_eosio.sh")
+		except Exception as e:
+			print("An error occurred: ", e)
 
 def simulate(blockchain):
 	build_chain(blockchain)
+	attack(blockchain)
+	end(blockchain)
